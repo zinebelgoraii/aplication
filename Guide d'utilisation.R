4c69ccb00971f408e1fef05e081148a0c6862a5d
zinebelgoraii <Zineb.ELGORAI-EXT@um6p.ma>
2024-05-20 00:03:15 +0100

    10 commit

---
+++ Guide d'utilisation.R
@@ -0,0 +1,17 @@
+library(devtools)
+install_github("zinebelgoraii/aplication")
+library(SDM3)
+
+dataset_path <- "loooooooooop/Datasets/xy.csv"
+shapefile_path <- "loooooooooop/Datasets/morocco.shp"
+output_folder <- "output"
+input_file <- "loooooooooop/predicteur/predictors_masked.tif"
+
+data_list <- SDM3::data_vis(dataset_path, shapefile_path, output_folder)
+train_test_data <- SDM3::data_train(data_list,input_file)
+
+mx<-SDM3::predict_maxent(train_test_data)
+rf<-SDM3::predict_rf(train_test_data)
+xgb<-SDM3::predict_xgboost(train_test_data)
+
+SDM3::evaluate_models(mx,rf,xgb,output_folder)
